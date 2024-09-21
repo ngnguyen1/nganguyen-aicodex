5071542ae8f8fd44f58de0be68b4e0749e7ffc75
Nga Nguyen <nga.nguyen@silabs.com>
2024-09-21 12:11:50 +0700

    fix lint

--- Codelab/Session01-02/calculator/api/controller.js
+++ Codelab/Session01-02/calculator/api/controller.js
@@ -61,16 +61,16 @@ exports.calculate = function(req, res) {
   }
 
   if (!req.query.operand1 ||
-      !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
-    throw new Error("Invalid operand1: " + req.query.operand1);
-  }
+    !req.query.operand1.match(/^(-)?[0-9.]+(e(-)?[0-9]+)?$/) || // Removed unnecessary escape character
+    req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
+  throw new Error("Invalid operand1: " + req.query.operand1);
+}
 
-  if (!req.query.operand2 ||
-      !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
-    throw new Error("Invalid operand2: " + req.query.operand2);
-  }
+if (!req.query.operand2 ||
+    !req.query.operand2.match(/^(-)?[0-9.]+(e(-)?[0-9]+)?$/) || // Removed unnecessary escape character
+    req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
+  throw new Error("Invalid operand2: " + req.query.operand2);
+}
 
   res.json({ result: operation(req.query.operand1, req.query.operand2) });
 };
